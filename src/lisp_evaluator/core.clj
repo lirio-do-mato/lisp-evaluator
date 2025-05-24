6c4c26ebb8a36c3eb05aecbcd3f4d9c0538884dd
Ian Pinheiro <ianzinho2004@gmail.com>
2025-05-24 00:45:12 -0300

    [ian] Create main loop with exit command.

---
+++ src/lisp_evaluator/core.clj
@@ -0,0 +1,18 @@
+(ns lisp-evaluator.core
+  (:gen-class))
+
+(defn -main
+  "My first Clojure project: a LISP evaluator."
+  []
+  (println "Welcome to my first Clojure project!")
+  (println "This is a simple LISP evaluator.")
+  (loop []
+    (print "Please enter a LISP expression (or 'exit' to quit): ") (flush)
+    (let [input (read-line)]
+      (when (= input "exit")
+        (println "Thank you for using the LISP evaluator!")
+        (println "Exiting the program...")
+        (Thread/sleep 1000)
+        (println "Goodbye!")
+        (System/exit 0))
+      (recur))))
