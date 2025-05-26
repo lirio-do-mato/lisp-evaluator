aff0e7bf2a72ab8447caba9ffa7c2aecb96f12d2
Ian Pinheiro <ianzinho2004@gmail.com>
2025-05-25 21:09:45 -0300

    [ian] Extract exit function and format code.

--- src/lisp_evaluator/core.clj
+++ src/lisp_evaluator/core.clj
@@ -1,18 +1,19 @@
 (ns lisp-evaluator.core
   (:gen-class))
 
-(defn -main
-  "My first Clojure project: a LISP evaluator."
-  []
+(defn exit-program "Exits the program with a goodbye message." [] 
+  (println "Thank you for using the LISP evaluator!")
+  (println "Exiting the program...")
+  (Thread/sleep 1000)  
+  (println "Goodbye!")  
+  (System/exit 0))
+
+(defn -main "My first Clojure project: a LISP evaluator." []
   (println "Welcome to my first Clojure project!")
   (println "This is a simple LISP evaluator.")
   (loop []
     (print "Please enter a LISP expression (or 'exit' to quit): ") (flush)
     (let [input (read-line)]
-      (when (= input "exit")
-        (println "Thank you for using the LISP evaluator!")
-        (println "Exiting the program...")
-        (Thread/sleep 1000)
-        (println "Goodbye!")
-        (System/exit 0))
+      (when (= input "exit") 
+        (exit-program))
       (recur))))
